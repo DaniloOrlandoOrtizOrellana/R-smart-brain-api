afbfccf128e6d76c430ff9eddc1fbb081a79bedc
DanilOrtiz <42508140+DanilOrtiz@users.noreply.github.com>
2020-04-20 12:05:00 -0600

    My first commit 20_04_2020

---
+++ server2.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+//app.use(express.static(__dirname + '/public'))
+
+/*app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json());
+
+app.get('/:id', (req, res) => {
+	//	console.log(req.query)
+	//req.body
+	//console.log(req.header);
+	console.log(req.params)
+	res.status(404).send("not found");
+});
+*/
+/*app.get('/profile', (req, res) => {
+	res.send("getting profile");
+});
+
+app.post('/profile', (req, res) => {
+
+	console.log(req.body)
+	const user = {
+		name: 'Sally',
+		hobby: 'soccer'
+	}
+	res.send('Success');
+});*/
+
+app.listen(3000);
